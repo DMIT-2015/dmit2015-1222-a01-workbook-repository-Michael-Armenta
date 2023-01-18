29e10ed5660cb7efe01955b29d2a59f5294bd717
Michael-Armenta <marmenta1@nait.ca>
2023-01-17 19:29:53 -0700

    created circle demo project

---
+++ dmit2015-azure-webapp/JUnitTestCase.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+@Test
+public class JUnitTestCase {
+    public void testForResult() {
+        SomeClass bean = new SomeClass();
+        bean.setSomeProperty();
+        assertEquals()
+    }
+}
