4ca2d6c06f091570e25c163c1665587ad3ffe8ad
Michael-Armenta <marmenta1@nait.ca>
2023-02-10 10:33:07 -0700

    Created videogame project to read and create files to a database

--- dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianIncomeTaxManager.java
@@ -2,18 +2,17 @@ package dmit2015.model;
 
 import lombok.Getter;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CanadianIncomeTaxManager {
 
-    // Define a private constructor to implement a Single Pattern
+    // Define a private constructor to implement Single pattern
     private CanadianIncomeTaxManager() {
-
+        loadDataFromFile();
     }
 
     // Define a single instance of this class
@@ -21,7 +20,7 @@ public class CanadianIncomeTaxManager {
 
     // Define a static class-level to access the singleton
     public static CanadianIncomeTaxManager getInstance() {
-        if(INSTANCE == null) {
+        if (INSTANCE == null) {
             INSTANCE = new CanadianIncomeTaxManager();
         }
         return INSTANCE;
@@ -30,16 +29,33 @@ public class CanadianIncomeTaxManager {
     @Getter
     private List<CanadianPersonalIncomeTaxRate> incomeTaxRates;
 
+    public int[] availableTaxYears() {
+        return incomeTaxRates.stream()
+                .map(item -> item.getTaxYear())
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(item -> item)
+                .toArray();
+    }
     public void loadDataFromFile() {
-        List<String> csvfile = new ArrayList();
         try {
-            Path csvPath = Path.of(
-                    getClass()
-                            .getClassLoader()
-                            .getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
-            csvfile = Files.readAllLines(csvPath);
-        } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(e);
+            try (var reader = new BufferedReader(new InputStreamReader(
+                    getClass().getResourceAsStream("/data/csv/CanadianPersonalIncomeTaxRates.csv")))) {
+                // Skip the first line as it contains headers
+                reader.readLine();
+                String line;
+                incomeTaxRates = new ArrayList<>();
+                while ( (line = reader.readLine()) != null) {
+                    var optionalCanadianPersonalIncomeTaxRate = CanadianPersonalIncomeTaxRate.parseCsv(line);
+                    if (optionalCanadianPersonalIncomeTaxRate.isPresent()) {
+                        CanadianPersonalIncomeTaxRate currentCanadianPersonalIncomeTaxRate
+                                = optionalCanadianPersonalIncomeTaxRate.orElseThrow();
+                        incomeTaxRates.add(currentCanadianPersonalIncomeTaxRate);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
         }
     }
 }
